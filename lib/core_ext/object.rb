0867627f89aaf54b115aac152b85c93ff53d03ae
Victor Ivanov <xuily@yandex.ru>
2009-10-30 21:19:39 +0300

    Made Ruby 1.9 compatible

--- lib/core_ext/object.rb
+++ lib/core_ext/object.rb
@@ -24,22 +24,28 @@ class Object
     result
   end
 
-  module InstanceExecHelper; end
-  include InstanceExecHelper
-  def instance_exec(*args, &block)
-    begin
-      old_critical, Thread.critical = Thread.critical, true
-      n = 0
-      n += 1 while respond_to?(mname="__instance_exec#{n}")
-      InstanceExecHelper.module_eval{ define_method(mname, &block) }
-    ensure
-      Thread.critical = old_critical
+  if RUBY_VERSION <= '1.9.1' then
+
+    module InstanceExecHelper; end
+    include InstanceExecHelper
+    def instance_exec(*args, &block)
+      if defined? instance_exec then
+         instance_exec(*args, &block)
+      end
+      begin
+        old_critical, Thread.critical = Thread.critical, true
+        n = 0
+        n += 1 while respond_to?(mname="__instance_exec#{n}")
+        InstanceExecHelper.module_eval{ define_method(mname, &block) }
+      ensure
+        Thread.critical = old_critical
+      end
+      begin
+        ret = send(mname, *args)
+      ensure
+        InstanceExecHelper.module_eval{ remove_method(mname) } rescue nil
+      end
+      ret
     end
-    begin
-      ret = send(mname, *args)
-    ensure
-      InstanceExecHelper.module_eval{ remove_method(mname) } rescue nil
-    end
-    ret
   end
 end
